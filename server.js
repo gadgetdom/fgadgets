a8fc1d0a3d1457c2a12bc571926079f58b0c3c34
Fidelis <phyd3lis@yandex.com>
2025-02-24 05:10:51 +0100

    baseURL update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/db.json'); // Ensure db.json is in the root directory
+const router = jsonServer.router('db.json'); // Ensure db.json is in the root directory
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
