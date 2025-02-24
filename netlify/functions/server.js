54d00deefe67c98cd5b35ed1408fba434c197ae9
Fidelis <phyd3lis@yandex.com>
2025-02-24 06:22:20 +0100

    Configured Netlify backend API

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async (event, context) => {
-    const dbPath = path.join(__dirname, "db.json");
+    const dbPath = path.join(__dirname, "/db.json");
 
     try {
         const data = fs.readFileSync(dbPath, "utf8");
